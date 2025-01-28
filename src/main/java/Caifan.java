546025e07edf71734a20a7d56b2d30c34d45ebf7
ljunfun <junfunlim@gmail.com>
2025-01-28 16:58:13 +0800

    Add Task class Add markTask, unmarkTask Update listTask with status icon

--- src/main/java/Caifan.java
+++ src/main/java/Caifan.java
@@ -1,34 +1,61 @@
 import java.util.Scanner;
 
 public class Caifan {
-    private static String[] taskList = new String[100];
+    private static Task[] taskList = new Task[100];
     private static int taskCount = 0;
 
     public static void printLine() {
         String line = "\t____________________________________________________________";
         System.out.println(line);
     }
+
     public static void sayHello() {
         String welcomePhrase =  "\tHello! I'm Caifan!\n" + "\tWhat can I do for you?";
         System.out.println(welcomePhrase);
     }
+
     public static void sayGoodbye() {
-        String goodbyePhrase = "\tBye. Hope to see you again soon <3";
+        String goodbyePhrase = "\tBye byeee!! Hope to see you again soon <3";
         System.out.println(goodbyePhrase);
     }
 
     public static void addTask(String line) {
         printLine();
-        System.out.println("\tadded this for you mi love: " + line);
+        System.out.println("\tadded this for you my love: " + line);
         printLine();
     }
 
+    //list down tasks with its status
     public static void listTasks() {
         printLine();
+        System.out.println("\tHere is your To-do List!!");
         for (int i = 0; i < taskCount; i++) {
-            System.out.println("\t" + (i + 1) + ". " + taskList[i]);
+            Task task = taskList[i];
+            System.out.println("\t" + (i + 1) + ".["+task.getStatusIcon()+"] " + task.getDescription());
         }
         printLine();
+    }
+
+    //set task as complete
+    public static void markTask(int index) {
+        if (index >= 0 && index < taskCount) {
+            taskList[index].setDone(true);
+            printLine();
+            System.out.println("\tYAY!! You have completed it :D");
+            System.out.println("\t  " + "["+taskList[index].getStatusIcon()+"] " + taskList[index].getDescription());
+            printLine();
+        }
+    }
+
+    //set task as incomplete
+    public static void unmarkTask(int index) {
+        if (index >= 0 && index < taskCount) {
+            taskList[index].setDone(false);
+            printLine();
+            System.out.println("\tGet your lazy ass up and finish this ><");
+            System.out.println("\t  " + "["+taskList[index].getStatusIcon()+"] " + taskList[index].getDescription());
+            printLine();
+        }
     }
 
     public static void main(String[] args) {
@@ -42,15 +69,21 @@ public class Caifan {
 
         while (true) {
             line = in.nextLine();
-            if (line.equalsIgnoreCase("bye")) {
+            if (line.equalsIgnoreCase("bye")) { //goodbye message
                 printLine();
                 sayGoodbye();
                 printLine();
                 break;
-            }else if (line.equalsIgnoreCase("list")) {
+            } else if (line.equalsIgnoreCase("list")) { //prints the list
                 listTasks();
-            } else {
-                taskList[taskCount] = line;
+            } else if (line.startsWith("mark ")) {
+                int index = Integer.parseInt(line.substring(5)) - 1;
+                markTask(index);
+            } else if (line.startsWith("unmark ")) {
+                int index = Integer.parseInt(line.substring(7)) - 1;
+                unmarkTask(index);
+            } else { //add task to list and track number of tasks
+                taskList[taskCount] = new Task(line);
                 taskCount++;
                 addTask(line);
             }
---
+++ src/main/java/Task.java
@@ -0,0 +1,23 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    // mark done task with ✓
+    public String getStatusIcon() {
+        return (isDone ? "✓" : " ");
+    }
+
+    //change the status of current task
+    public void setDone(boolean status) {
+        isDone = status;
+    }
+}
